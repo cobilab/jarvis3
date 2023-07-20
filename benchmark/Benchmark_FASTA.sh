4c341cde8d39f4dfe48b4ce9f0fd8714bb7a16c8
pratas <6882743+pratas@users.noreply.github.com>
2023-07-20 14:51:18 +0100

    bench fasta

---
+++ benchmark/Benchmark_FASTA.sh
@@ -0,0 +1,536 @@
+#!/bin/bash
+#
+# ==============================================================================
+#
+CHECK_INPUT () {
+  FILE=$1
+  if [ -f "$FILE" ];
+    then
+    echo "Input filename: $FILE"
+    else
+    echo -e "\e[31mERROR: input file not found ($FILE)!\e[0m";
+    exit;
+    fi
+  }
+#
+# ==============================================================================
+#
+function RUN_GECO3 {
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND $IN_FILE \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  BYTES=`ls -la $IN_FILE.co | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND $IN_FILE.co \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $IN_FILE.de $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_GECO2 {
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND $IN_FILE \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  BYTES=`ls -la $IN_FILE.co | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND $IN_FILE.co \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $IN_FILE.de $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_JARVIS2_BIN {
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND $IN_FILE \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  BYTES=`ls -la $IN_FILE.jc | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND $IN_FILE.jc \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $IN_FILE.jc.jd $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_JARVIS3_BIN {
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND $IN_FILE \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  BYTES=`ls -la $IN_FILE.jc | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND $IN_FILE.jc \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $IN_FILE.jc.jd $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_JARVIS1 {
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND $IN_FILE \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  BYTES=`ls -la $IN_FILE.jc | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND $IN_FILE.jc \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $IN_FILE.jc.jd $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_JARVIS2_SH {
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" ./JARVIS2.sh --fasta --level " $C_COMMAND " $6 --input $IN_FILE \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  BYTES=`ls -la $IN_FILE.tar | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" ./JARVIS2.sh $D_COMMAND $IN_FILE.tar \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $IN_FILE.tar.out $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_JARVIS3_SH {
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" ./JARVIS3.sh --level " $C_COMMAND " $6 --input $IN_FILE \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  BYTES=`ls -la $IN_FILE.tar | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" ./JARVIS3.sh $D_COMMAND $IN_FILE.tar \
+  |& grep "TIME" \
+  |& tr '.' ',' \
+  |& awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $IN_FILE.tar.out $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_NAF {
+  #
+  rm -fr tmp 
+  mkdir -p tmp/
+  #
+  IN_FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+#  C_NAF_FILE=${IN_FILE/.fa/.naf};
+#  D_NAF_FILE=${IN_FILE/.fa/.unnaf};
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND -o NAF-OUT.naf $IN_FILE 2> naf_tmp_report.txt;
+  cat naf_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  #
+  BYTES=`ls -la NAF-OUT.naf | awk '{ print $5 }'`;
+  echo "BYTES=$BYTES";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND -o NAD-D-OUT.naf NAF-OUT.naf 2> naf_tmp_report.txt 
+  cat naf_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp NAD-D-OUT.naf $IN_FILE > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  rm -f NAF-OUT.naf NAF-D-OUT.naf;
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_LZMA {
+  #
+  FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  cp $FILE $FILE.orig
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND $FILE.orig 2> c_tmp_report.txt;
+  cat c_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  #
+  BYTES=`ls -la $FILE.orig.lzma | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND $FILE.orig.lzma 2> d_tmp_report.txt
+  cat d_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $FILE $FILE.orig > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  rm -f $FILE.orig $FILE.orig.lzma c_tmp_report.txt d_tmp_report.txt c_time_mem.txt d_time_mem.txt
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_BZIP2 {
+  #
+  FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  cp $FILE $FILE.orig
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND $FILE.orig 2> c_tmp_report.txt;
+  cat c_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  #
+  BYTES=`ls -la $FILE.orig.bz2 | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND $FILE.orig.bz2 2> d_tmp_report.txt
+  cat d_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $FILE $FILE.orig > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  rm -f $FILE.orig $FILE.orig.bz2 c_tmp_report.txt d_tmp_report.txt c_time_mem.txt d_time_mem.txt
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_BSC {
+  #
+  FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  cp $FILE $FILE.orig
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" ./bsc-m03 e $FILE.orig $FILE.bsc $C_COMMAND 1> c_stdout.txt 2> c_tmp_report.txt;
+  cat c_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  #
+  BYTES=`ls -la $FILE.bsc | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND d $FILE.bsc $FILE.out 1> d_stdout.txt 2> d_tmp_report.txt
+  cat d_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $FILE.out $FILE.orig > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  rm -f $FILE.orig $FILE.bsc $FILE.out c_tmp_report.txt d_tmp_report.txt c_time_mem.txt d_time_mem.txt c_stdout.txt d_stdout.txt
+  #
+  }
+#
+# ==============================================================================
+#
+function RUN_MFC {
+  #
+  FILE="$1";
+  C_COMMAND="$2";
+  D_COMMAND="$3";
+  NAME="$4";
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $C_COMMAND -o MFC-OUT.mfc $FILE 1> c_stdout.txt 2> c_tmp_report.txt;
+  cat c_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > c_time_mem.txt;
+  #
+  BYTES=`ls -la MFC-OUT.mfc | awk '{ print $5 }'`;
+  #
+  /bin/time -f "TIME\t%e\tMEM\t%M" $D_COMMAND -o MFC-OUT.d MFC-OUT.mfc 1> d_stdout.txt 2> d_tmp_report.txt
+  cat d_tmp_report.txt | grep "TIME" | tr '.' ',' | awk '{ printf $2/60"\t"$4/1024/1024"\n" }' > d_time_mem.txt;
+  #
+  cmp $FILE MFC-OUT.d > cmp.txt;
+  #
+  C_TIME=`cat c_time_mem.txt | awk '{ print $1}'`;
+  C_MEME=`cat c_time_mem.txt | awk '{ print $2}'`;
+  D_TIME=`cat d_time_mem.txt | awk '{ print $1}'`;
+  D_MEME=`cat d_time_mem.txt | awk '{ print $2}'`;
+  VERIFY="0";
+  CMP_SIZE=`ls -la cmp.txt | awk '{ print $5}'`
+  if [[ "$CMP_SIZE" != "0" ]]; then CMP_SIZE="1"; fi
+  #
+  printf "$NAME\t$BYTES\t$C_TIME\t$C_MEME\t$D_TIME\t$D_MEME\t$CMP_SIZE\t$5\n";
+  #
+  rm -f MFC-OUT.mfc MFC-OUT.d c_tmp_report.txt d_tmp_report.txt c_time_mem.txt d_time_mem.txt c_stdout.txt d_stdout.txt
+  #
+  }
+#
+
+# ==============================================================================
+#
+FILE="$1";
+#
+rm -f data.csv
+#
+# ==============================================================================
+#
+printf "PROGRAM\tC_BYTES\tC_TIME (m)\tC_MEM (GB)\tD_TIME (m)\tD_MEM (GB)\tDIFF\tRUN\n";
+#
+# ------------------------------------------------------------------------------
+#
+#RUN_GECO2 "$FILE" "GeCo2 -v -l 1 " "GeDe2 -v " "GeCo2" "1" >> data.csv;
+#RUN_GECO2 "$FILE" "GeCo2 -v -l 5 " "GeDe2 -v " "GeCo2" "1" >> data.csv;
+#RUN_GECO2 "$FILE" "GeCo2 -v -l 7 " "GeDe2 -v " "GeCo2" "1" >> data.csv;
+#RUN_GECO3 "$FILE" "GeCo3 -v -l 1" "GeDe3 -v " "GeCo3" "6" >> data.csv;
+#RUN_GECO3 "$FILE" "GeCo3 -v -l 5" "GeDe3 -v " "GeCo3" "6" >> data.csv;
+#RUN_GECO3 "$FILE" "GeCo3 -v -l 7" "GeDe3 -v " "GeCo3" "6" >> data.csv;
+#RUN_JARVIS1 "$FILE" "JARVIS -v -rm 2000:12:0.1:0.9:6:0.10:1 -cm 4:1:1:0.7/0:0:0:0 -z 6 " "JARVIS -d " "JARVIS1" "15" >> data.csv;
+#RUN_JARVIS2_BIN "$FILE" "./JARVIS2 -v -l 1 " "./JARVIS2 -d " "JARVIS2-bin" "16" >> data.csv;
+#RUN_JARVIS2_SH "$FILE" " -lr 0.01 -hs 42 -rm 200:11:1:0.9:7:0.3:1:0.2:220000 -cm 12:1:1:0.85/0:0:0:0 " " --decompress --threads 8 --dna --input " "JARVIS2-sh" "23" " --block 100MB --threads 8 --dna "
+RUN_NAF "$FILE" "ennaf --temp-dir tmp/ --dna --level 2 " "unnaf " "NAF" "24" >> data.csv;
+RUN_NAF "$FILE" "ennaf --temp-dir tmp/ --dna --level 12 " "unnaf " "NAF" "24" >> data.csv;
+RUN_NAF "$FILE" "ennaf --temp-dir tmp/ --dna --level 22 " "unnaf " "NAF" "24" >> data.csv;
+RUN_LZMA "$FILE" "lzma -1 -f -k " "lzma -f -k -d " "LZMA" "25" >> data.csv;
+RUN_LZMA "$FILE" "lzma -5 -f -k " "lzma -f -k -d " "LZMA" "25" >> data.csv;
+RUN_LZMA "$FILE" "lzma -9 -f -k " "lzma -f -k -d " "LZMA" "25" >> data.csv;
+RUN_BZIP2 "$FILE" "bzip2 -1 -f -k " "bzip2 -f -k -d " "BZIP2" "26" >> data.csv;
+RUN_BZIP2 "$FILE" "bzip2 -5 -f -k " "bzip2 -f -k -d " "BZIP2" "26" >> data.csv;
+RUN_BZIP2 "$FILE" "bzip2 -9 -f -k " "bzip2 -f -k -d " "BZIP2" "26" >> data.csv;
+#RUN_BSC "$FILE" " -b800000000 " "./bsc-m03 " "BSC-m03" "27" >> data.csv;
+#RUN_BSC "$FILE" " -b400000000 " "./bsc-m03 " "BSC-m03" "28" >> data.csv;
+RUN_BSC "$FILE" " -b4096000 " "./bsc-m03 " "BSC-m03" "29" >> data.csv;
+RUN_MFC "$FILE" "./MFCompressC -v -1 -p 1 -t 1 " "./MFCompressD " "MFCompress" "30" >> data.csv;
+RUN_MFC "$FILE" "./MFCompressC -v -2 -p 1 -t 1 " "./MFCompressD " "MFCompress" "31" >> data.csv;
+RUN_MFC "$FILE" "./MFCompressC -v -3 -p 1 -t 1 " "./MFCompressD " "MFCompress" "32" >> data.csv;
+#
+for((x=1;x<=5;++x)); 
+do
+echo "Running $x...";
+#RUN_JARVIS3_BIN "$FILE" "./JARVIS3 -v -l $x " "./JARVIS3 -d " "JARVIS3" "33" >> data.csv;
+RUN_JARVIS3_SH "$FILE" " $x --block 600MB --threads 1 " " --decompress --threads 1 --fasta --input " "JARVIS3" "23" " --block 600MB --threads 1 --fasta " >> data.csv;
+done
+#
+cat data.csv;
+#
+# ==============================================================================
+#
+CHECK_INPUT "data.csv";
+cat data.csv | tr ',' '.' > data2.csv
+cat data2.csv | awk '{ print $1} ' | sort -V | uniq > names.txt
+SIZE=`wc -l names.txt | awk '{ print $1;}'`;
+CHECK_INPUT "names.txt";
+#
+idx=1;
+plotnames="";
+mapfile -t INT_DATA < names.txt;
+for dint in "${INT_DATA[@]}"
+  do
+  #
+  grep $dint data2.csv > data-$idx.csv
+  tmp="'data-$idx.csv' u 2:3 w lp ls $idx title '$dint', ";
+  plotnames="$plotnames $tmp";
+  ((++idx));
+  #
+  done
+#
+echo "$plotnames";
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,12'
+    set output "Benchmark.pdf"
+    set style line 101 lc rgb '#000000' lt 1 lw 2
+    set border 3 front ls 101
+    set tics nomirror out scale 0.75
+    set key fixed right top vertical Right noreverse noenhanced autotitle nobox
+    set style histogram clustered gap 1 title textcolor lt -1
+    set xtics border in scale 0,0 nomirror #rotate by -60  autojustify
+    set yrange [:]
+    set xrange [:]
+    set xtics auto
+    set ytics auto
+    set key top right
+    #set style line 4 lc rgb '#CC0000' lt 2 dashtype '---' lw 4 pt 5 ps 0.4 # --- red
+    set style line 1 lc rgb '#990099'  pt 1 ps 0.6  # circle
+    set style line 2 lc rgb '#004C99'  pt 2 ps 0.6  # circle
+    set style line 3 lc rgb '#CCCC00'  pt 3 ps 0.6  # circle
+    set style line 4 lc rgb 'red'  pt 7 ps 0.6  # circle
+    set style line 5 lc rgb '#009900'  pt 5 ps 0.6  # circle
+    set style line 6 lc rgb '#990000'  pt 6 ps 0.6  # circle
+    set style line 7 lc rgb '#009999'  pt 4 ps 0.6  # circle
+    set style line 8 lc rgb '#19004C'  pt 8 ps 0.6  # circle
+    set style line 9 lc rgb '#CC6600'  pt 9 ps 0.6  # circle
+    set style line 10 lc rgb '#322152' pt 10 ps 0.6  # circle
+    set style line 11 lc rgb '#425152' pt 11 ps 0.6  # circle
+    set grid
+    set ylabel "Real time (minutes)"
+    set xlabel "Data compressed size (bytes)"
+    plot $plotnames
+EOF
+#
+evince Benchmark.pdf &
+#
+# ==============================================================================
