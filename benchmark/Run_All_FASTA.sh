5b02ee83f16ce365be21e6045f6327361bad87d2
pratas <6882743+pratas@users.noreply.github.com>
2023-07-27 14:29:28 +0100

    run all

--- benchmark/Run_All_FASTA.sh
+++ benchmark/Run_All_FASTA.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 #
-wget -o CASSAVA.fa https://ftp.cngb.org/pub/gigadb/pub/10.5524/102001_103000/102193/00_Assembly_Fasta/haplotigs/TME204.HiFi_HiC.haplotig1.fa
-#
-./Install_Tools.sh
-#
 ./Benchmark_FASTA.sh VIRUS_REFS_NCBI_15_JULY.fa
 ./Benchmark_FASTA.sh CY.fa
 ./Benchmark_FASTA.sh CASSAVA.fa
